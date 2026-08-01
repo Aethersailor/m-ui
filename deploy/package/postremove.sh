b7de072f3a1b5423780469de8b54f13b5ec5f531
Aethersailor <22260104+Aethersailor@users.noreply.github.com>
2026-08-02 06:06:15 +0800

    fix(packaging): preserve files during package upgrade

--- deploy/package/postremove.sh
+++ deploy/package/postremove.sh
@@ -2,17 +2,21 @@
 
 set -eu
 
-rm -f \
-    /usr/bin/m-ui \
-    /etc/systemd/system/m-ui.service \
-    /etc/systemd/system/mihomo.service \
-    /lib/systemd/system/m-ui.service \
-    /lib/systemd/system/mihomo.service \
-    /etc/sudoers.d/m-ui \
-    /etc/init.d/m-ui \
-    /etc/init.d/mihomo \
-    /etc/doas.d/m-ui.conf
-rm -rf /usr/lib/m-ui /usr/share/m-ui
+case "${1:-}" in
+    remove|purge)
+        rm -f \
+            /usr/bin/m-ui \
+            /etc/systemd/system/m-ui.service \
+            /etc/systemd/system/mihomo.service \
+            /lib/systemd/system/m-ui.service \
+            /lib/systemd/system/mihomo.service \
+            /etc/sudoers.d/m-ui \
+            /etc/init.d/m-ui \
+            /etc/init.d/mihomo \
+            /etc/doas.d/m-ui.conf
+        rm -rf /usr/lib/m-ui /usr/share/m-ui
+        ;;
+esac
 
 if [ -d /run/systemd/system ]; then
     systemctl daemon-reload
